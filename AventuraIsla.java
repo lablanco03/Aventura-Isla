9bc812a4a7e772ce6d2b5301d82380577c04edad
Alejandro Blanco <lablanco03@gmail.com>
2025-03-28 11:08:38 -0600

    Todo listo para comenzar a agregar los acertijos

--- AventuraIsla.java
+++ AventuraIsla.java
@@ -28,56 +28,50 @@
      public static void main(String[] args) throws IOException {
          
         //DECLARACIÓN DE VARIABLES
-        int location = 1, action, capacidad = 0, jugando = 1;
-        String inventario = "", objeto01 = "", objeto02 = "", objeto03 = "";
+        int location = 1, action, jugando = 1, puntos = 0;
+        String inventario = "", objeto01 = "", objeto02 = "", objeto03 = "", objeto04 = "", respuestaAdivinanzaCueva;
     
-        while (jugando == 1) {
-            
-            //MENSAJE AL INICIAR EL VIDEOJUEGO
-            out.println("\n¡BIENVENIDO AL JUEGO DE LA AVENTURA EN ISLA DEL TESORO!\n");
+        //MENSAJE AL INICIAR EL VIDEOJUEGO
+        out.println("\n¡BIENVENIDO AL JUEGO DE LA AVENTURA EN ISLA DEL TESORO!");
+
+        while (jugando == 1) { 
             
             //PLAYA
-            if (location == 1){
-            out.println("--------------------\nEstás en la playa y es hora de avanzar.\nEl sol brilla, escuchas las olas reventar contra la arena y ves un cofre abandonado cerca de ti");
-            out.println("\n¿Qué deseas hacer?");
-            out.println("1. Ir al Norte\n2. Examinar el cofre");
-            out.print("\nIngrese el número que determinará su acción: ");
+            if (location == 1) {
+            out.println("\n--------------------\n\nEstás en la playa y lo ideal es avanzar.\nEl sol brilla, escuchas las olas reventar contra la arena y ves un cofre abandonado cerca de ti.");
+            out.println("\n¿Qué deseas hacer?\n");
+            out.println("1. Ir al Norte\n2. Examinar el cofre\n3. Salir del juego");
+            out.print("\nIngrese el número que determinará tu acción: ");
             action = Integer.parseInt(in.readLine());
 
                 //ACCIONES QUE SE PUEDEN HACER EN LA PLAYA
                 switch (action) {
-                    case 1 -> {
-                        location = 2; //JUNGLA
-                    }
+                    case 1 -> {location = 2;} //JUNGLA
                     case 2 -> {
                         if (inventario.contains("| Mapa |")) {
-                            out.println("\n--------------------\nYa guardaste el mapa en tu inventario.");
+                            out.println("\n--------------------\n\nYa guardaste el mapa en tu inventario.");
                             out.println("Actualmente este es tu inventario: " + inventario);
                         }
-                        else if (capacidad < 3) {
-                            out.println("\n--------------------\n¡Hallaste un mapa dentro del cofre!");
+                        else {
+                            out.println("\n--------------------\n\n¡Hallaste un mapa dentro del cofre!");
                             objeto01 += "| Mapa |";
                             inventario += objeto01;
                             out.println("Actualmente este es tu inventario: " + inventario);
                             out.println("\nEl mapa muestra lo siguiente:\nExiste una densa jungla en el centro de la isla,\nal norte hay unas ruinas antiguas,\nal oeste una gran cueva,\nal este una inmensa montaña,\nal sur la playa en la que te encuentras.");
-                            out.println("\n¡Recuerda bien estas instrucciones!\n¡Avancemos a la siguiente área!");
-                            capacidad++;   
+                            out.println("\n¡Recuerda bien estas instrucciones!"); 
                         } 
-                        else if (capacidad == 3){
-                            out.println("\n--------------------\nYa llevas 3 objetos. No tienes más espacio en tu inventario");
-                        }
-                        location = 2; //JUNGLA
                     }
-                    default -> {jugando = 0;}
+                    case 3 -> {jugando = 0;} //SALIR
+                    default -> {out.println("\n--------------------\n\nIngresaste una opción inválida. ¡Intenta de nuevo!");}
                 }
             }
 
             //JUNGLA
-            if (location == 2){
-                out.println("\n--------------------\nSeguiste la ruta disponible, avanzaste al norte y llegaste a la jungla.\nLos árboles son altos y la vegetación frondosa, pero ves caminos en todas direcciones.");
-                out.println("\n¿Qué deseas hacer?");
-                out.println("1. Ir al Norte\n2. Ir al Oeste\n3. Ir al Este\n4. Ir al Sur");
-                out.print("\nIngrese el número que determinará su acción: ");
+            if (location == 2) {
+                out.println("\n--------------------\n\nLlegaste a la jungla.\nLos árboles son altos y la vegetación frondosa. Ves caminos en todas direcciones.");
+                out.println("\n¿Qué deseas hacer?\n");
+                out.println("1. Ir al Norte\n2. Ir al Oeste\n3. Ir al Este\n4. Ir al Sur\n5. Salir del juego");
+                out.print("\nIngrese el número que determinará tu acción: ");
                 action = Integer.parseInt(in.readLine());
 
                 //ACCIONES QUE SE PUEDEN HACER EN LA JUNGLA
@@ -86,76 +80,64 @@
                     case 2 -> {location = 3;} //CUEVA
                     case 3 -> {location = 5;} //MONTAÑA
                     case 4 -> {location = 1;} //PLAYA
-                    default -> {jugando = 0;}
+                    case 5 -> {jugando = 0;}  //SALIR
+                    default -> {out.println("\n--------------------\n\nIngresaste una opción inválida. ¡Intenta de nuevo!");}
                 }
             }
 
             //CUEVA
-            if (location == 3) 
-            {
-                out.println("\n--------------------\nAvanzaste al oeste, ahora estás en la entrada de una cueva.\nEstá muy oscuro, oyes ruidos extraños, pero no ves bien.");
-                out.println("\n¿Qué deseas hacer?");
-                out.println("1. Ir al Este\n2. Intentar ir dentro de la cueva");
-                out.print("\nIngrese el número que determinará su acción: ");
+            if (location == 3) {
+                out.println("\n--------------------\n\nEstás en la entrada de una cueva.\nMiras hacia dentro, oyes ruidos extraños, pero está muy oscuro y no ves bien.");
+                out.println("\n¿Qué deseas hacer?\n");
+                out.println("1. Ir al Este\n2. Intentar ir dentro de la cueva\n3. Salir del juego");
+                out.print("\nIngrese el número que determinará tu acción: ");
                 action = Integer.parseInt(in.readLine());
 
                 //ACCIONES QUE SE PUEDEN HACER EN LA CUEVA
                 switch (action) {
                     case 1 -> {location = 2;} //JUNGLA
-                    case 2 -> {
-                        out.print("Está muy oscuro, será mejor regresar. Quizá más adelante halles algo que te pueda servir para ver mejor.");
-
-                        location = 2; //JUNGLA
-                    }
-                    default -> {jugando = 0;}
+                    case 2 -> {location = 6;} //ACERTIJO
+                    case 3 -> {jugando = 0;}  //SALIR
+                    default -> {out.println("\n--------------------\n\nIngresaste una opción inválida. ¡Intenta de nuevo!");}
                 }
             }
 
             //RUINAS
-            if (location == 4) 
-            {
-                out.println("\n--------------------\nSeguiste al norte y llegaste a unas ruinas.\nParecen antiguas y tienen unos símbolos extraños escritos en sus paredes.");
-                out.println("\n¿Qué deseas hacer?");
-                out.println("1. Ir al Sur\n2. Recoger objeto en la entrada");
-                out.print("\nIngrese el número que determinará su acción: ");
+            if (location == 4) {
+                out.println("\n--------------------\n\nLlegaste a unas ruinas.\nParecen antiguas y tienen unos símbolos extraños escritos en sus paredes.");
+                out.println("\n¿Qué deseas hacer?\n");
+                out.println("1. Ir al Sur\n2. Recoger objeto en la entrada\n3. Examinar símbolos\n4. Salir del juego");
+                out.print("\nIngrese el número que determinará tu acción: ");
                 action = Integer.parseInt(in.readLine());
 
                 //ACCIONES QUE SE PUEDEN HACER EN LAS RUINAS
                 switch (action) {
-                    case 1 -> {
-                        location = 2; //JUNGLA
-                    }
+                    case 1 -> {location = 2;} //JUNGLA
                     case 2 -> {
                         if (inventario.contains("| Antorcha |")) {
-                            out.println("\n--------------------\nYa guardaste el mapa en tu inventario.");
+                            out.println("\n--------------------\n\nYa guardaste la antorcha en tu inventario.");
                             out.println("Actualmente este es tu inventario: " + inventario);
                         }
-                        else if (capacidad < 3) {
-                            out.println("\n--------------------\n¡Recogiste una antorcha!");
-                            
+                        else {
+                            out.println("\n--------------------\n\n¡Recogiste una antorcha!");
                             objeto02 += "| Antorcha |";
                             inventario += objeto02;
                             out.println("Actualmente este es tu inventario: " + inventario);
-                            out.println("\nPuedes usarla para iluminar espacios oscuros o calentarte si hace frío.\n¡Te será muy útil en ciertos sitios!");
-                            out.println("Por ahora regresemos a la anterior zona.");
-                            capacidad++;   
+                            out.println("\nPuedes usarla para iluminar espacios oscuros o calentarte si hace frío.\n¡Te será muy útil en ciertos sitios!");   
                         } 
-                        else if (capacidad == 3){
-                            out.println("\n--------------------\nYa llevas 3 objetos. No tienes más espacio en tu inventario");
-                        }
-                        location = 2; //JUNGLA
                     }
-                    default -> {jugando = 0;}
+                    case 3 -> {location = 7;} //ACERTIJO
+                    case 4 -> {jugando = 0;} //SALIR
+                    default -> {out.println("\n--------------------\n\nIngresaste una opción inválida. ¡Intenta de nuevo!");}
                 }
             }
 
             //MONTAÑA
-            if (location == 5)
-            {
-                out.println("\n--------------------\nDecidiste ir al este, ahora estás justo en la base de una montaña.\nSe ve muy grande y puede que halla algo interesante arriba o al menos verás un buen paisaje.");
-                out.println("\n¿Qué deseas hacer?");
-                out.println("1. Ir al Oeste\n2. Examinar objeto clavado en el suelo");
-                out.print("\nIngrese el número que determinará su acción: ");
+            if (location == 5) {
+                out.println("\n--------------------\n\nEstás estás justo en la base de una montaña.\nSe ve muy grande y puede que halla algo interesante arriba o al menos verás un buen paisaje.");
+                out.println("\n¿Qué deseas hacer?\n");
+                out.println("1. Ir al Oeste\n2. Examinar objeto clavado en el suelo\n3. Intentar escalar\n4. Salir del juego");
+                out.print("\nIngrese el número que determinará tu acción: ");
                 action = Integer.parseInt(in.readLine());
 
                 //ACCIONES QUE SE PUEDEN HACER EN LA MONTAÑA
@@ -163,32 +145,30 @@
                     case 1 -> {location = 2;} //JUNGLA
                     case 2 -> {
                         if (inventario.contains("| Pala |")) {
-                            out.println("\n--------------------\nYa guardaste el mapa en tu inventario.");
+                            out.println("\n--------------------\n\nYa guardaste la pala en tu inventario.");
                             out.println("Actualmente este es tu inventario: " + inventario);
                         }
-                        else if (capacidad < 3) {
-                            out.println("\n--------------------\n¡Hallaste una pala!");
+                        else {
+                            out.println("\n--------------------\n\n¡Hallaste una pala!");
                             objeto03 += "| Pala |";
                             inventario += objeto03;
                             out.println("Actualmente este es tu inventario: " + inventario);
                             out.println("\nAhora podrás excavar en lugares donde puede que halla algo valioso oculto.\n¡Puede serte útil en ciertas zonas!");
-                            out.println("Regresemos y sigamos explorando.");
-                            capacidad++;   
-                        } 
-                        else if (capacidad == 3){
-                            out.println("\n--------------------\nYa llevas 3 objetos. No tienes más espacio en tu inventario");
+                            out.println("Regresemos y sigamos explorando.");  
                         }
-                        location = 2; //JUNGLA
                     }
-                    default -> {jugando = 0;}
+                    case 3 -> {location = 8;} //ACERTIJO
+                    case 4 -> {jugando = 0;}  //SALIR
+                    default -> {out.println("\n--------------------\n\nIngresaste una opción inválida. ¡Intenta de nuevo!");}
                 }
-            } 
+            }
             
-        }
+            //ACERTIJOS Y PUNTUACIÓN
+            
+            //Acá van los acertijos
 
+        }
         //MENSAJE DE DESPEDIDA DEL VIDEOJUEGO
-        out.println("\n--------------------\n¡Gracias por jugar! ¡Hasta la próxima!");
- 
+        out.println("\n--------------------\n\nGracias por jugar. ¡Hasta la próxima!");
     }
-    
 } //FIN DEL PROGRAMA
